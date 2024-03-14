9ffa06b526c036a9e4c13d885c0f5d64b25de17f
PotassiumG7 <101847887+PotasiumG7@users.noreply.github.com>
2024-03-13 18:56:08 -0700

    Update constants.js

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -1,82 +1,82 @@
-import tamiscal from 'assets/images/tiles/tamiscal.png';
-import marinAcademy from 'assets/images/tiles/ma.png';
-import sanMarin from 'assets/images/tiles/sanMarin.png';
-import terraLinda from 'assets/images/tiles/terraLinda.png';
-import redwood from 'assets/images/tiles/redwood.png';
-import novato from 'assets/images/tiles/novato.png';
-import archieWilliams from 'assets/images/tiles/archie.jpeg';
-import marinCatholic from 'assets/images/tiles/mc.png';
-import branson from 'assets/images/tiles/branson.png';
-import sanRafael from 'assets/images/tiles/sanRafael.jpeg';
-import tamHigh from 'assets/images/tiles/tam.png';
+import tamiscal from 'assets/images/tiles/bek.jpg';
+import marinAcademy from 'assets/images/tiles/arg.jpg';
+import sanMarin from 'assets/images/tiles/bart.jpg';
+import terraLinda from 'assets/images/tiles/jan.jpg';
+import redwood from 'assets/images/tiles/himes.jpg';
+import novato from 'assets/images/tiles/hals.jpg';
+import archieWilliams from 'assets/images/tiles/banaag.jpg';
+import marinCatholic from 'assets/images/tiles/cole.jpg';
+import branson from 'assets/images/tiles/mat.jpg';
+import sanRafael from 'assets/images/tiles/allen.jpg';
+import tamHigh from 'assets/images/tiles/stocker.jpg';
 
 let constants = {
 	damping: 0.1,
 	tiles: [
 		{
-			name: 'Tamiscal',
-			imagePath: 'tamiscal.png',
-			image: tamiscal,
+			name: 'Becker',
+			imagePath: 'bek.jpg',
+			image: becker,
 			worth: 2,
 		},
 		{
-			name: 'Marin Academy',
-			imagePath: 'ma.png',
-			image: marinAcademy,
+			name: 'Arrigo',
+			imagePath: 'arg.jpg',
+			image: arrigo,
 			worth: 4,
 		},
 		{
-			name: 'San Marin',
-			imagePath: 'sanMarin.png',
-			image: sanMarin,
+			name: 'Barret',
+			imagePath: 'bart.jpg',
+			image: barret,
 			worth: 8,
 		},
 		{
-			name: 'Terra Linda',
-			imagePath: 'terraLinda.png',
-			image: terraLinda,
+			name: 'Janusch',
+			imagePath: 'jan.jpg',
+			image: janusch,
 			worth: 16,
 		},
 		{
-			name: 'Redwood',
-			imagePath: 'redwood.png',
-			image: redwood,
+			name: 'Himes',
+			imagePath: 'himes.jpg',
+			image: himes,
 			worth: 32,
 		},
 		{
-			name: 'Novato',
-			imagePath: 'novato.png',
-			image: novato,
+			name: 'Halstead',
+			imagePath: 'hals.jpg',
+			image: halstead,
 			worth: 64,
 		},
 		{
-			name: 'Archie Williams',
-			imagePath: 'archie.jpeg',
-			image: archieWilliams,
+			name: 'Banaag',
+			imagePath: 'banaag.jpg',
+			image: banaag,
 			worth: 128,
 		},
 		{
-			name: 'Marin Catholic',
-			imagePath: 'mc.png',
-			image: marinCatholic,
+			name: 'Coleman',
+			imagePath: 'cole.jpg',
+			image: coleman,
 			worth: 256,
 		},
 		{
-			name: 'San Rafael',
-			imagePath: 'sanRafael.jpeg',
-			image: sanRafael,
+			name: 'John Matern',
+			imagePath: 'mat.jpg',
+			image: johnMatern,
 			worth: 512,
 		},
 		{
-			name: 'Branson',
-			imagePath: 'branson.png',
-			image: branson,
+			name: 'Amy Allen',
+			imagePath: 'allen.jpg',
+			image: amyAllen,
 			worth: 1024,
 		},
 		{
-			name: 'Tam High',
-			imagePath: 'tam.png',
-			image: tamHigh,
+			name: 'Christy Stocker',
+			imagePath: 'stocker.jpg',
+			image: christyStocker,
 			worth: 2048,
 		},
 	],
